88ea7e529ef39895fa7c2ec7c213b92617128667
VIDYA VENUGOPAL <98008921+vidyaven@users.noreply.github.com>
2022-06-09 01:04:04 -0700

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,25 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { SearchFilterPipe } from './search-filter.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DemoComponent,
+    SearchFilterPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
